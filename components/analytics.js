15388de4b21bc118b14791e34316344522e9484a
Nate Gadzhi <nate@respawn.io>
2022-06-22 18:07:23 -0700

    analytics

---
+++ components/analytics.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Script from 'next/script'
+
+export default function Analytics() {
+  return (
+    <Head>
+      <Script src='https://www.googletagmanager.com/gtag/js?id=G-0KK7Z9WP3' strategy='afterInteractive' />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+          gtag('config', 'G-0KK7Z9WP39');
+        `}
+      </Script>
+    </Head>
+  )
+}
--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,12 @@
 import Footer from '../components/footer'
 import Meta from '../components/meta'
+import Analytics from '../components/analytics'
 
 export default function Layout({ children }) {
   return (
     <>
       <Meta />
+      <Analytics />
       <div className="min-h-screen max-w-3xl mx-auto">
         <main>{children}</main>
       </div>
