611029bcf5938bf75439da2c302cce075d07243f
Nate Gadzhi <nate@respawn.io>
2022-07-03 22:47:12 -0700

    footer font size fix

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ import { config } from '../blog.config'
 export default function Footer() {
   return (
     <footer className="border-t border-accent-2 py-4">
-      <div className="text-sm max-w-3xl mx-auto px-5">
+      <div className="text-md max-w-3xl mx-auto px-5">
         {config.footer.copyright} {new Date().getFullYear()}
       </div>
     </footer>
