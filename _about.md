5ad0018d541d7f83912a9e76c57cfe5207477297
Nate Gadzhi <nate@respawn.io>
2022-07-15 22:47:11 -0700

    Simple about page

---
+++ _about.md
@@ -0,0 +1,17 @@
+# Hi! I'm Nate
+
+I'm a director of engineering at [Zipline](https://getzipline.com). Before that, I've spent 10 years at
+[Evil Martians](https://evl.ms).
+
+I'm a builder at heart, and a software engineer by training, and I'm fascinated by how people learn, and organize into
+large groups to build things together.
+
+I write about engineering leadership, building software, and learning. The source code for this blog, along with all the
+essays in Markdown, are [available on Github](https://github.com/nategadzhi/respawn-io).
+
+Get in touch:
+
+- Email: nate@respawn.io
+- Twitter: [@nategadzi](https://twitter.com/nategadzhi)
+- [GitHub](https://github.com/nategadzhi)
+- [Linkedin](https://linkedin.com/in/nategadzhi)
--- pages/about.js
+++ pages/about.js
@@ -3,9 +3,15 @@ import Layout from '../components/layout'
 import Header from '../components/header'
 import Head from 'next/head'
 
+
+import fs from 'fs'
+import { join } from 'path'
+import markdownToHtml from '../lib/markdownToHtml'
+import markdownStyles from '../components/markdown-styles.module.css'
+
 import { config } from '../blog.config'
 
-export default function About() {
+export default function About({ about }) {
   return (
     <>
       <Layout>
@@ -14,11 +20,25 @@ export default function About() {
         </Head>
         <Container>
           <Header />
-          <section className='max-w-2xl mx-auto'>
-            About
+          <section className='prose'>
+            <div
+              className={markdownStyles['markdown']}
+              dangerouslySetInnerHTML={{ __html: about }}
+            />
           </section>
         </Container>
       </Layout>
     </>
   )
+}
+
+
+export async function getStaticProps() {
+  const aboutPath = join(process.cwd(), '_about.md')
+  const about = await markdownToHtml(fs.readFileSync(aboutPath, 'utf8'))
+  return {
+    props: {
+      about: about
+    },
+  }
 }
