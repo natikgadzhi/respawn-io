6a134854af6a578e0c710d0f347189c7a383d90b
Nate Gadzhi <nate@respawn.io>
2022-07-15 22:13:41 -0700

    Added page nav

--- components/header.js
+++ components/header.js
@@ -6,13 +6,26 @@ export default function Header() {
   const router = useRouter()
 
   return (
-    <section className="flex-col flex items-center mt-16 mb-16 md:mb-12">
-      <h2 className="text-2xl md:text-3xl font-bold tracking-tighter leading-tight md:pr-8">
-        { router.asPath === '/' ? config.title : <Link href="/"><a className="hover:underline">{config.title}</a></Link> }
-      </h2>
-      <p className="text-center md:text-left text-md mt-5 md:pl-8">
-        { config.description }
-      </p>
+    <section className='flex justify-between justify-items-stretch flex-nowrap items-baseline my-8 md:my-12 space-x-8' >
+      <div>
+        <h2 className="text-xl md:text-2xl font-bold tracking-tighter leading-tight md:mb-2">
+          { router.asPath === '/' ? config.title : <Link href="/"><a className="hover:underline">{config.title}</a></Link> }
+        </h2>
+        <p className="text-left text-md">
+          { config.description }
+        </p>
+      </div>
+
+      <div>
+        <ul className='text-md md:text-lg space-x-4'>
+          <li className='inline-block'>
+            <Link href="/about"><a className='hover:underline'>About</a></Link>
+          </li>
+          <li className='inline-block'>
+            <Link href="/rss/feed.xml"><a className='hover:underline'>RSS</a></Link>
+          </li>
+        </ul>
+      </div>
     </section>
   )
 }
