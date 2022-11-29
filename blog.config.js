15057c5420dee466e4a84d7df79ead1f50d82f26
Nate Gadzhi <nate@respawn.io>
2022-11-28 18:04:09 -0800

    Name cleanup: Nate -> Natik

--- blog.config.js
+++ blog.config.js
@@ -1,22 +1,22 @@
 // These are some config settings used across the markup and website.
 
 export const config = {
-  title: "Nate's Journal",
+  title: "Natik's Journal",
   description: "Notes on enginering leadership and software development.",
   baseURL: "https://respawn.io",
-  githubURL: "https://github.com/nategadzhi/respawn-io",
+  githubURL: "https://github.com/natikgadzhi/respawn-io",
   meta: {
     description: "A blog on engineering leadership and software development.",
     ogImage: ""
   },
 
   author: {
-    name: "Nate Gadzhi",
+    name: "Natik Gadzhi",
     email: "nate@respawn.io",
-    twitterURL: "https://twitter.com/nategadzhi",
+    twitterURL: "https://twitter.com/natikgadzhi",
   },
 
   footer: {
-    copyright: "© Nate Gadzhi",
+    copyright: "© Natik Gadzhi",
   }
 }
