d5b76f443751d265cac69ecf4cf2ad31f467a464
Natik Gadzhi <natik@respawn.io>
2023-06-01 15:43:27 +0400

    Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,11 @@
 import { AppProps } from "next/app";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 import { DefaultSeo } from "next-seo";
-import * as Fathom from "fathom-client";
+import { Analytics } from "@vercel/analytics/react";
 
 import "../styles/index.css";
 import { config as blogConfig } from "../blog.config";
 
 function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Initialize Fathom when the app loads
-    Fathom.load(blogConfig.fathom.propertyID, {
-      includedDomains: [blogConfig.fathom.domain],
-      url: blogConfig.fathom.scriptURL,
-    });
-
-    function onRouteChangeComplete() {
-      Fathom.trackPageview();
-    }
-    // Record a pageview when route changes
-    router.events.on("routeChangeComplete", onRouteChangeComplete);
-
-    // Unassign event listener
-    return () => {
-      router.events.off("routeChangeComplete", onRouteChangeComplete);
-    };
-  });
-
   return (
     <>
       <DefaultSeo
@@ -45,6 +22,7 @@ function App({ Component, pageProps }: AppProps) {
           }}
         />
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
