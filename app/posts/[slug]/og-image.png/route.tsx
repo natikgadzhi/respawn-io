ce126f626ea70e3403c0d7954b55ad0c1c65374f
Natik Gadzhi <natik@respawn.io>
2023-06-09 23:46:27 +0400

    Fixed font imports in og image endpoint

--- app/posts/[slug]/og-image.png/route.tsx
+++ app/posts/[slug]/og-image.png/route.tsx
@@ -7,30 +7,26 @@ import OpengraphImage from "app/posts/[slug]/og-image.png/image";
 
 export const runtime = "edge";
 
-// Helper func to load a font from the file system
-// and pass it into the `ImageResponseOptions` below.
-const loadFont = async (fontName: string): Promise<ArrayBuffer> => {
-  const res = await fetch(new URL(`assets/fonts/${fontName}`, import.meta.url));
-  return await res.arrayBuffer();
-};
 
 
 // `ImageResponseOptions` are passed into `ImageResponse` alongside the
 // component to actually draw the image. It contains width, height, and fonts.
-const imageResponseOptions = async (width: number, height: number): Promise<ImageResponseOptions> => {
+async function imageResponseOptions(width: number, height: number): Promise<ImageResponseOptions> {
   return {
     width: width,
     height: height,
     fonts: [
       {
         name: "Jetbrains Mono",
-        data: await loadFont("JetBrainsMono-Regular.ttf"),
+        data: await fetch(new URL(`assets/fonts/JetBrainsMono-Regular.ttf`, import.meta.url))
+          .then((res) => res.arrayBuffer()),
         style: "normal",
         weight: 400,
       },
       {
         name: "Jetbrains Mono",
-        data: await loadFont("JetBrainsMono-ExtraBold.ttf"),
+        data: await fetch(new URL(`assets/fonts/JetBrainsMono-ExtraBold.ttf`, import.meta.url))
+          .then((res) => res.arrayBuffer()),
         style: "normal",
         weight: 800,
       },
