be820aaf7c10098bf7db026fbf9f0255b2201ed9
Natik Gadzhi <natik@respawn.io>
2023-06-09 21:33:30 +0400

    Formatting

--- components/callout.tsx
+++ components/callout.tsx
@@ -1,16 +1,15 @@
-
 type Props = {
-    children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 const Callout = ({ children }: Props) => {
-    return (
-        <div className="not-prose rounded-md px-8 py-4 bg-amber-100 shadow-sm">
-            <p className="leading-relaxed font-serif italic tracking-tight">
-                { children }
-            </p>
-        </div>
-    )
-}
+  return (
+    <div className="not-prose rounded-md px-8 py-4 bg-amber-100 shadow-sm">
+      <p className="leading-relaxed font-serif italic tracking-tight">
+        {children}
+      </p>
+    </div>
+  );
+};
 
 export default Callout;
