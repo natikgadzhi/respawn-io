03a3b7703fec3094646d3dfc5c25bd4b842e8b17
Natik Gadzhi <natik@respawn.io>
2023-06-13 16:44:49 +0400

    No footer border

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import { config } from '../blog.config'
 
 const Footer = () => {
   return (
-    <footer className="border-t border-accent-2 py-4">
+    <footer className="py-4">
       <div className="text-md max-w-3xl mx-auto px-5">
         {config.footer.copyright} {new Date().getFullYear()}
       </div>
