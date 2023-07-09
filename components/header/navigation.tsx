4e94c2e8edfecdafe53649aadd271c61319b91bc
Natik Gadzhi <natik@respawn.io>
2023-07-09 16:48:40 -0700

    Daily notes page in navigation

--- components/header/navigation.tsx
+++ components/header/navigation.tsx
@@ -8,6 +8,9 @@ const Navigation = ({ className }: Props) => {
     return (
         <ul className={`${className} text-md md:text-lg flex-row justify-start space-x-4 text-blue-700 dark:text-sky-500`}>
           <li className='inline-block'>
+            <Link href="/daily" className='hover:underline underline-offset-4'>TIL</Link>
+          </li>
+          <li className='inline-block'>
             <Link href="/about" className='hover: hover:underline underline-offset-4'>About</Link>
           </li>
           <li className='inline-block'>
