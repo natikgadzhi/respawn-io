dc451689d17424e21e55eebb9aa2cdbf59beead1
Natik Gadzhi <natik@respawn.io>
2023-11-08 16:22:06 -0800

    Autolinked headings

--- lib/mdxComponents.tsx
+++ lib/mdxComponents.tsx
@@ -1,15 +1,45 @@
 import Link from "next/link";
 import Image from "next/image";
 import Callout from "components/callout";
+import { LinkIcon } from "components/icons";
 
 import type { MDXComponents } from "mdx/types";
 
+const HeaderLink = ({ id }: { id: string }) => (
+  <Link
+    href={`#${id}`}
+    className="not-prose hidden md:inline-block absolute -left-10 top-9 p-1
+      opacity-70 hover:opacity-100 transition-opacity duration-100
+      hover:outline hover:outline-offset-1 hover:outline-1 hover:rounded-md
+    hover:outline-stone-500 hover:dark:outline-blue-100">
+    <LinkIcon />
+  </Link>
+);
+
 export const mdxComponents: MDXComponents = {
-  a: ({ href, children }) => (
-    <Link href={href as string} className="decoration-1 hover:decoration-2">
-      {children}
-    </Link>
-  ),
+  h2: ({ children }) => {
+    const id = children.toString().toLowerCase().replace(/[^\w]/g, "-");
+    return (
+      <>
+        <h2 id={id} className="relative">
+          <HeaderLink id={id} />
+          {children}
+        </h2>
+      </>
+    );
+  },
+  h3: ({ children }) => {
+    const id = children.toString().toLowerCase().replace(/[^\w]/g, "-");
+    return (
+      <>
+        <h3 id={id} className="relative">
+          <HeaderLink id={id} />
+          {children}
+        </h3>
+      </>
+    );
+  },
+  a: ({ href, children }) => <Link href={href as string}>{children}</Link>,
   img: ({ src, alt }) => (
     <span className="inline-block relative w-full pb-[56.25%] max-h-[800px]">
       <Image
