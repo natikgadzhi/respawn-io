47b86e2524189a2ea90df9ab95cfb66230592601
Natik Gadzhi <natik@respawn.io>
2023-11-19 09:56:02 -0800

    BUGFIX: Truncate long post titles in OG image

--- app/posts/[slug]/og-image.png/image.tsx
+++ app/posts/[slug]/og-image.png/image.tsx
@@ -4,9 +4,9 @@ type Props = {
   post: Post;
   width: number;
   height: number;
-}
+};
 
-export default function OpengraphImage({ post, width, height}: Props) {
+export default function OpengraphImage({ post, width, height }: Props) {
   return (
     <>
       <div
@@ -16,29 +16,33 @@ export default function OpengraphImage({ post, width, height}: Props) {
           fontFamily: '"Jetbrains Mono"',
           backgroundImage: "linear-gradient(90deg, #cbd5e1 0%, #f8fafc 100%)",
         }}
-        tw="flex flex-col justify-start w-full h-full p-12 px-24 bg-slate-50">
-
-        <h1 style={{
-          backgroundImage: "linear-gradient(90deg, #1d4ed8 0%, #581c87 100%)",
-          backgroundClip: "text",
-          /* @ts-ignore */
-          "-webkit-background-clip": "text",
-          color: "transparent",
-        }}
-          tw="text-7xl break-keep font-extrabold mb-8">
+        tw="flex flex-col justify-start w-full h-full p-12 px-24 bg-slate-50"
+      >
+        <h1
+          style={{
+            backgroundImage: "linear-gradient(90deg, #1d4ed8 0%, #581c87 100%)",
+            backgroundClip: "text",
+            /* @ts-ignore */
+            "-webkit-background-clip": "text",
+            color: "transparent",
+          }}
+          tw="text-7xl break-keep font-extrabold mb-8"
+        >
           {post.formattedTitle}
         </h1>
 
-        <p tw="mt-4 text-4xl font-extrabold leading-normal">{post.excerpt}</p>
+        {post.formattedTitle.length < 60 && (
+          <p tw="mt-4 text-4xl font-extrabold leading-normal">{post.excerpt}</p>
+        )}
 
-        <div tw="flex mt-auto flex-row justify-between items-baseline w-full">
-          <a
-            href={post.absoluteURL}
-            tw="text-2xl text-blue-700 ml-auto">
-            {post.absoluteURL}
-          </a>
-        </div>
+        {post.formattedTitle.length < 60 && (
+          <div tw="flex mt-auto flex-row justify-between items-baseline w-full">
+            <a href={post.absoluteURL} tw="text-2xl text-blue-700 ml-auto">
+              {post.absoluteURL}
+            </a>
+          </div>
+        )}
       </div>
     </>
   );
-};
+}
