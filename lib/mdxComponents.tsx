cf73fa4748aee87cf8016eb53ed70b3038d9b932
Natik Gadzhi <natik@respawn.io>
2024-11-15 14:22:08 -0800

    Switch to pnpm and update types

--- lib/mdxComponents.tsx
+++ lib/mdxComponents.tsx
@@ -5,12 +5,11 @@ import { titleCase } from "./titleCase";
 
 import type { MDXComponents } from "mdx/types";
 
-
 export const mdxComponents: MDXComponents = {
   // While post titles are title cased automatically in `post.formattedTitle`,
   // the <Article /> will just pass the whole body of the post into MDX, and
-  // the body of the post has the title in h1 in the first line. 
-  // 
+  // the body of the post has the title in h1 in the first line.
+  //
   // Plus, we generally want titles to be title cased, so why not.
   // TODO: Do we want this for h2 as well?
   h1: ({ children }) => <h1>{titleCase(children)}</h1>,
