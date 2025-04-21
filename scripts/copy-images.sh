8c374c0767be54cf55ea9dc2c208b2b6c9ffab49
Natik Gadzhi <natik@respawn.io>
2025-04-20 17:13:53 -0700

    post: natik's user manual

--- scripts/copy-images.sh
+++ scripts/copy-images.sh
@@ -9,8 +9,8 @@ dest_dir="public"
 # Create the destination directory if it does not exist
 mkdir -p $dest_dir
 
-# Find all .png files in the source directory and its sub-directories
-find $source_dir -name "*.png" | while read file; do
+# Find all .png and .jpg files in the source directory and its sub-directories
+find $source_dir -name "*.png" -o -name "*.jpg" | while read file; do
   # Replace the first occurrence of $source_dir with $dest_dir
   dest="${file/$source_dir/$dest_dir}"
 
