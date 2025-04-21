f60d4132db9d3f66057e615c58b28994bc83e11f
Natik Gadzhi <natik@respawn.io>
2025-04-20 17:34:54 -0700

    copy daily images too

--- scripts/copy-images.sh
+++ scripts/copy-images.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-# The directory we are copying from
-source_dir="content/posts"
-
-# The directory we are copying to
-dest_dir="public"
-
-# Create the destination directory if it does not exist
-mkdir -p $dest_dir
-
-# Find all .png and .jpg files in the source directory and its sub-directories
-find $source_dir -name "*.png" -o -name "*.jpg" | while read file; do
-  # Replace the first occurrence of $source_dir with $dest_dir
-  dest="${file/$source_dir/$dest_dir}"
+# Copy function to handle each content type directory
+copy_images() {
+  local source_dir=$1
+  local dest_dir="public"
 
   # Create the destination directory if it does not exist
-  mkdir -p "$(dirname "$dest")"
+  mkdir -p $dest_dir
 
-  # Copy the file
-  cp "$file" "$dest"
-done
+  # Find all .png and .jpg files in the source directory and its sub-directories
+  find $source_dir -name "*.png" -o -name "*.jpg" | while read file; do
+    # Replace the first occurrence of $source_dir with $dest_dir
+    dest="${file/$source_dir/$dest_dir}"
+
+    # Create the destination directory if it does not exist
+    mkdir -p "$(dirname "$dest")"
+
+    # Copy the file
+    cp "$file" "$dest"
+  done
+}
+
+# Process posts
+copy_images "content/posts"
+
+# Process daily notes
+copy_images "content/daily"
